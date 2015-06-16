a2ea4f68b338c099663fad11c21557a75037d4bd
Eric Liou <qxesqxes@gmail.com>
2015-06-16 11:08:51 +0800

    fix png image bug of js/glass.js

--- public/js/glass.js
+++ public/js/glass.js
@@ -37,6 +37,7 @@ var Glass = {
 	};
     } ,
     copyImg:function( x, y, w, h ) {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	this.context.arc( 150, 150 , 150 , 0 , Math.PI * 2 , true);
 	this.context.clip();
 	this.context.drawImage( this.img , x , y , w, h , 0 , 0, 300 ,300 );
